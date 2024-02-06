1923a7285026c9c8b35db7c0f8cef9e24c5ff00f
sahal <sahalshalu830@gmail.com>
2024-02-06 13:43:24 +0530

    sales report ready with pdf

--- backend/employee/ChartData/urls.py
+++ backend/employee/ChartData/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/usercount/',UserCountApiView.as_view(),name='user_count'),
     path('admin/booking_detials/',BookingDetialsApi.as_view(),name='booking_detials'),
     path('booking_report_emp/<int:employee_id>/', BookingReportEmployeeApi.as_view(),name='booking_report_emp'),
-    path('sales_report/', SalesReportView.as_view(),name='SalesReportView'),
+    path('sales_report/<int:employee>/', SalesReportView.as_view(),name='SalesReportView'),
+    path('sales-report-pdf/<int:employee>/', SalesReportPDFView.as_view(), name='sales_report_pdf'),
 ]
--- backend/employee/ChartData/views.py
+++ backend/employee/ChartData/views.py
@@ -1,12 +1,16 @@
 from employee.models import EmployeeBooking
 from auth_setup.models import User
 from rest_framework import generics
-from django.http import Http404, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.db.models import Sum,Count
 from .serializers import BookingEmployeeReportSerializer
 from employee.serializers import EmployeeBookingSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import datetime
+from django.http import HttpResponse
+from fpdf import FPDF
+from rest_framework import generics
 #total users count
 class UserCountApiView(generics.RetrieveAPIView):
     def get(self,request,*args, **kwargs):
@@ -57,9 +61,78 @@ class BookingReportEmployeeApi(generics.RetrieveUpdateAPIView):
 class SalesReportView(generics.ListAPIView):
     serializer_class = EmployeeBookingSerializer
     def get_queryset(self):
-
+        employee = self.kwargs.get('employee')
+        status = self.request.query_params.get('booking_status')
         start_date = self.request.query_params.get('start_date')
         end_date = self.request.query_params.get('end_date')
-        if start_date and end_date and start_date != end_date:
-            queryset = EmployeeBooking.objects.filter(created_date__range=(start_date,end_date))
+
+        if status=='all' and  start_date=='' and end_date=='':
+            queryset = EmployeeBooking.objects.filter(employee=employee)
             return queryset
+        
+        elif status!= 'all'  and start_date != ''  and end_date!= '':
+            queryset = EmployeeBooking.objects.filter(created_date__range=(start_date,end_date),employee=employee,booking_status=status)
+            return queryset
+        
+        elif status== 'all'  and start_date != ''  and end_date!= '':
+            queryset = EmployeeBooking.objects.filter(created_date__range=(start_date,end_date),employee=employee)
+            return queryset
+        elif status!= 'all'  and start_date == ''  and end_date== '':
+            queryset = EmployeeBooking.objects.filter(booking_status=status,employee=employee)
+            return queryset
+
+class SalesReportPDFView(generics.ListAPIView):
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        response = HttpResponse(content_type='application/pdf')
+        filename = 'Expenses_' + str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + '.pdf'
+        response['Content-Disposition'] = 'attachment; filename="' + filename + '"'
+
+        w_pt =8.5*50
+        h_pt =11*20
+        pdf = FPDF(format=(w_pt,h_pt))
+        pdf.set_auto_page_break(auto=True, margin=15)
+
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.cell(200, 10, txt="Sales Report", ln=True, align='C')
+        pdf.cell(200, 10, txt=str(datetime.now()), ln=True, align='C')
+        col_names = ['Id', 'Client', 'Email', 'Phone number', 'Booking date', 'Price', 'Status']
+        col_widths = [20, 40, 70, 40, 30, 30, 40]
+        for i, name in enumerate(col_names):
+            pdf.cell(col_widths[i], 10, name, border=1)
+        pdf.ln()
+        for obj in queryset:
+            serializer = EmployeeBookingSerializer(obj)
+            data_row = [
+                serializer.data['id'],
+                serializer.data['userDetails']['username'],
+                serializer.data['userDetails']['email'],
+                serializer.data['userDetails']['phone_number'],
+                serializer.data['booking_date'],
+                serializer.data['price'],
+                serializer.data['booking_status']
+            ]
+            for i, item in enumerate(data_row):
+                pdf.cell(col_widths[i], 10, str(item), border=1)
+            pdf.ln()
+
+        response.write(pdf.output(dest='S').encode('latin1'))
+        return response
+
+    def get_queryset(self):
+        employee = self.kwargs.get('employee')
+        status = self.request.query_params.get('booking_status')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+
+        queryset = EmployeeBooking.objects.filter(employee=employee)
+
+        if status != 'all':
+            queryset = queryset.filter(booking_status=status)
+        if start_date and end_date:
+            queryset = queryset.filter(created_date__range=(start_date, end_date))
+
+        return queryset
+
+        
